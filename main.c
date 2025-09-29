ebfe970e3b49f2e5d7b91d83f0808a828a4448fa
IvanGst <12999537@itisrossi.vi.it>
2025-09-29 13:25:53 +0200

    finale

--- main.c
+++ main.c
@@ -1,5 +1,47 @@
+#include <stdbool.h>
 #include <stdio.h>
 
+#include <stdio.h>
+#include <stdbool.h>
+
 int main(void) {
-    char stringa[]="i topi non avevano nipoti";
+    char stringa[] = "i topi non avevano nipoti";
+    bool palindromo = true;
+    char *testa = stringa;
+    char *coda = stringa;
+
+    while (*coda != '\0') {
+        coda++;
+    }
+    coda--;
+
+    while (testa < coda) {
+        if (*testa == ' ') {
+            testa++;
+            continue;
+        }
+        if (*coda == ' ') {
+            coda--;
+            continue;
+        }
+
+        if (*testa != *coda) {
+            palindromo = false;
+            break;
+        }
+
+        testa++;
+        coda--;
+    }
+
+    if (palindromo) {
+        printf("La stringa e un palindromo.\n");
+    } else {
+        printf("La stringa NON e un palindromo.\n");
+    }
+
+    return 0;
 }
+
+
+
